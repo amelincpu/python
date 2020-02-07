b830c8568f18e11f1a7aa68332097e442bdfd262
amelincpu <amelincpu@gmail.com>
2020-02-07 16:03:00 +0000

    new repository

---
+++ notes.py
@@ -0,0 +1,34 @@
+#github testing
+
+# git config --global user.name "amelincpu"
+# git config --global user.email "amelincpu@gmail.com"
+
+# https://github.com/amelincpu/test.git
+
+# 'git init' 
+# 'git add .' - add all files
+# 'git commit -m "description for commit"' - commit all changes
+# 'git remote add origin https://github.com/amelincpu/test.git' - set remote repository
+# 'git push -u origin master' - push files to the repository
+
+t1 = input('ss: ')
+
+t6 = 0
+if t6 == 4:
+    print('t6 equals 4')
+elif t6 == 0:
+   print('it is just null')
+else:
+    print('t6 does not equal 4')
+
+t7 = ['asdfgghytrew', 'dddddwewfw']
+print(len(t7[0]))
+if len(t7) != 0 and type(t7) == list:
+    print('it is a string')
+else:
+    print('it is not a string')
+	
+	
+
+
+    git remote add origin https://github.com/amelincpu/Python.git
