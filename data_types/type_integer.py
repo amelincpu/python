36723970051eed063ee9b481814d3c95387fd0b1
amelincpu <amelincpu@gmail.com>
2020-02-13 09:11:23 +0000

    folders creation 2

--- data_types/type_integer.py
+++ data_types/type_integer.py
@@ -1,14 +1,14 @@
 # integer is a numeric type
 var_a = 1
+print(var_a)
 
-# type() - data type checking
-type(var_a)
+type(var_a) # type() - data type checking
 print(type(var_a))
 
-# id() - memory id check
 
-id(var_a)
+id(var_a) # id() - memory id check
 print(id(var_a))
 
 var_a = var_a + 1
-print(id('memory is is:', var_a))
+print('memory is is:', id(var_a))
+
