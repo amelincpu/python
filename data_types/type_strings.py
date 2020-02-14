82de44042b8b1ecf8adf15d64ff4f842c92d4c41
amelincpu <amelincpu@gmail.com>
2020-02-14 09:14:36 +0000

    variables

--- data_types/type_strings.py
+++ data_types/type_strings.py
@@ -18,6 +18,26 @@ var_a = var_a + var_b
 print('var_a variable =', (var_a), '\na memory cell id for var_a variable is:', id(var_a))
 
 var_a = 'Hello World!' # returning original value of var_a
+print('\nvar_a variable =', (var_a))
 var_a[0] # access to a string elements from 0 until X
+print('\naccess to a string elements:')
 print(var_a[0])
 print(var_a[7])
+
+var_a[-3] # access to a string elements from the end
+print(var_a[-2])
+print(var_a[-4])
+
+var_a[-3] # access to a string range
+print(var_a[2:5])
+print(var_a[5:])
+print(var_a[5:-1])
+print(var_a[0:9:2]) # 3rd means only every second element from the range
+print(var_a[::-1]) # flip the string
+
+# string methods
+print('\nstring methods:')
+var_a_up = var_a.upper() # make all symbols upper case
+print(var_a_up)
+var_a_low = var_a.lower() # make all symbols lower case
+print(var_a_low)
