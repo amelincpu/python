0aace4ab5e09077a385f5912efac4e361354463b
amelincpu <amelincpu@gmail.com>
2020-02-27 15:56:31 +0000

    20200227 update 2

--- data_types/type_sets.py
+++ data_types/type_sets.py
@@ -11,3 +11,23 @@ print('var_a variable data type is:', type(var_a))
 print('\n', '*'*50, '\n')
 #
 # a list to a tuple converting - tuple(list_variable)
+var_b = [1, 2, 'hello', 'two', 1, 2, 2, 1, 'two']
+var_c = set(var_b) # only unique values come into the set
+print('var_b=', var_b)
+print('var_c = set(var_b)')
+print('var_c=', var_c)
+print('var_c variable data type is:', type(var_c))
+#
+print('\n', '*'*50, '\n')
+#
+# 'add' - a method to add a new element
+# 'discard' - a method to delete a element
+var_c.add(4)
+print('var_c.add(4)')
+print('var_c=', var_c)
+#
+var_c.discard('two')
+print("var_c.discard('two')")
+print('var_c=', var_c)
+#
+print('\n', '*'*50, '\n')
