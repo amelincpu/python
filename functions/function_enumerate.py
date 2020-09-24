80669d7df4c773a99aba673dfd1bf0a9b9b11c50
amelincpu <amelincpu@gmail.com>
2020-09-24 16:03:23 +0100

    24 Sep 2020 update

---
+++ functions/function_enumerate.py
@@ -0,0 +1,15 @@
+# Enumerate function.
+
+print('\n', '*'*50, '\n')
+
+# 'enumerate' returns index-item pairs
+v_list = [
+    ['vlan 23', 'Fa0/01', 'f123.d144.43dc'],
+    ['vlan 3', 'Fa0/05', 'fe23.de44.43dc'],
+    ['vlan 23', 'Fa0/13', 'a123.dee4.43dc']
+    ]
+
+for v_index, v_item in enumerate(v_list, 1):
+    print(v_index, v_item)
+
+print('\n', '*'*50, '\n')
--- functions/function_sorted.py
+++ functions/function_sorted.py
@@ -7,8 +7,8 @@ print(sorted((5, 6, 2, 9, 11)))
 print(sorted('kajifeufksqo'))
 
 v_list = [
-    [23, 'Fa0/01', 'f123.d144.43dc'],
-    [3, 'Fa0/05', 'fe23.de44.43dc'],
+    [23, 'Fa0/1', 'f123.d144.43dc'],
+    [3, 'Fa0/5', 'fe23.de44.43dc'],
     [23, 'Fa0/13', 'a123.dee4.43dc']
     ]
 print(sorted(v_list))
@@ -16,5 +16,12 @@ print(sorted(v_list))
 # import a feature to point to an item in a list, to call by index
 from operator import itemgetter
 print(sorted(v_list, key=itemgetter(1)))
+
+print('\n', '*'*50, '\n')
+
+# 'key=int' helps 'sorted' to work with integer
+v_vlans = ['23', '2', '12', '1', '10', '100']
+print(sorted(v_vlans))
+print(sorted(v_vlans, key=int))
 
 print('\n', '*'*50, '\n')
