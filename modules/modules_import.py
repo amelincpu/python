018c742187651f28ea3df16d71dd7c753fca706a
amelincpu <amelincpu@gmail.com>
2020-09-26 08:28:23 +0100

    26 September 2020 update

--- modules/modules_import.py
+++ modules/modules_import.py
@@ -4,15 +4,19 @@ print('\n', '*'*50, '\n')
 
 # import 'sys' module, access to the module functions via 'sys.*'
 import sys
-
 print(sys.version)
 
 print('\n', '*'*50, '\n')
 
 # import only 'version' function from 'sys' module, access to the function via 'version'
-
 from sys import version
-
 print(version)
+
+# import a few functions
+from sys import version, argv
+
+# assinge an alias for a module
+import sys as module_system
+print(module_system.version)
 
 print('\n', '*'*50, '\n')
