c3e445263c3bf2013e149ee8cf44cad93fab03e2
amelincpu <amelincpu@gmail.com>
2020-10-05 15:22:54 +0100

    5 October 2020 update

---
+++ regular_expressions/module_re-function_search.py
@@ -0,0 +1,28 @@
+import re
+
+print('\n', '*'*50, '\n')
+
+v_line ='Interface IP-Address OK? Method Status Protocol FastEthernet0/0 10.1.1.1 YES NVRAM up up Serial1/0 10.4.4.1 YES manual up up Serial1/1 unassigned YES unset administratively down down Serial1/2 unassigned YES unset administratively down down Serial1/3 unassigned YES unset administratively down down Loopback0 1.1.1.1 YES NVRAM up up Loopback1 11.11.11.11 YES NVRAM up up Loopback11 1.2.2.2 YES NVRAM up up Loopback12 1.3.3.3 YES NVRAM up up Loopback22 111.111.111.111 YES NVRAM up up'
+print(v_line)
+print(re.search('Loopback', v_line))
+
+print('\n', '*'*50, '\n')
+
+# 'search' function works utill the first hit
+v_re = re.search('\d+\.\d+\.\d+\.\d+', v_line)
+print(v_re.group())
+print(type(v_re))
+
+print('\n', '*'*50, '\n')
+
+v_re_line = []
+v_re_line = v_re.group()
+print(v_re_line)
+print(type(v_re_line))
+
+print('\n', '*'*50, '\n')
+
+match = re.search('\d\d.\d.\d.\d', v_line)
+print(match.group())
+
+print('\n', '*'*50, '\n')
---
+++ regular_expressions/re_syntaxis.py
@@ -0,0 +1,16 @@
+''' 
+-----------
+Groupe/set of symbols:
+
+\d - any digits
+\D - any symbole exclude digits
+\s - any whitespace (\t\n\r\f\v)
+\S - any symbols exclude whitespace
+\w - any letter or digit or _
+\W - any exclude letter or digit or _
+
+-----------
+
+
+
+ '''
