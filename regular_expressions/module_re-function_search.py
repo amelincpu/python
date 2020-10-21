a7abc642d9c0064d8988c31560abf95d46dbadfa
amelincpu <amelincpu@gmail.com>
2020-10-21 22:25:44 +0100

    21 October 2020 update

--- regular_expressions/module_re-function_search.py
+++ regular_expressions/module_re-function_search.py
@@ -11,21 +11,31 @@ print(re.search('Loopback', v_line), end='')
 
 print('\n', '*'*50, '\n', end='')
 
-# 'search' function works utill the first hit
+# # 'search' function works utill the first hit
 # v_re = re.search('\d+\.\d+\.\d+\.\d+', v_line)
 # print(v_re.group())
 # print(type(v_re))
 
 print('\n', '*'*50, '\n', end='')
 
-# v_re_line = []
 # v_re_line = v_re.group()
 # print(v_re_line)
 # print(type(v_re_line))
 
 print('\n', '*'*50, '\n', end='')
 
-# match = re.search('\d\d.\d.\d.\d', v_line)
+# match = re.search('\d{2}(.\d){3}', v_line)
 # print(match.group())
+
+print('\n', '*'*50, '\n',  end='')
+
+match = re.search('(Fast).*(Serial)', v_line)
+print(match.group(), '\n')
+print(match.groups())
+print(match.group(1))
+print(match.group(2))
+print(match.group(2, 1, 2))
+
+print(match[2])
 
 print('\n', '*'*50, '\n',  end='')
