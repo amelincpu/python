07f4c60d3129597ffb9589a0eb65195c95da1af8
amelincpu <amelincpu@gmail.com>
2021-01-28 17:42:39 +0000

    28.01.2021 update

--- sqlight/sqlight.py
+++ sqlight/sqlight.py
@@ -1,3 +1,5 @@
+"""."""
+
 import os
 import sys
 import sqlite3
@@ -15,14 +17,16 @@ for line in show_db:
 new_db = sqlite3.connect(os.path.join(sys.path[0], 'new.db'))
 db_operation = new_db.cursor()
 db_operation.execute("""CREATE TABLE IF NOT EXISTS switches (
-                        id integer PRIMARY KEY,
-	                    hostname text NOT NULL,
-                        model text,
-                        ip text)""")
-to_db = [(1, 'sw01', '2960', '1.1.1.1'), 
-        (2, 'sw02', '3750', '1.1.1.2'),
-        (3, 'sw03', '3850', '1.1.1.3'),
-        (4, 'sw04', '2960', '1.1.1.4')]
+                    id integer PRIMARY KEY,
+                    hostname text NOT NULL,
+                    model text,
+                    ip text)""")
+
+to_db = [(1, 'sw01', '2960', '1.1.1.1'),
+         (2, 'sw02', '3750', '1.1.1.2'),
+         (3, 'sw03', '3850', '1.1.1.3'),
+         (4, 'sw04', '2960', '1.1.1.4')]
+
 to_db_operation = "INSERT INTO switches values (?, ?, ?, ?)"
 db_operation.executemany(to_db_operation, to_db)
 new_db.commit()
--- sqlight/sqlight2.py
+++ sqlight/sqlight2.py
@@ -1,19 +1,28 @@
+"""."""
+
+
 import os
 import sys
 import sqlite3
 
+
 def print_db_table():
+    """Print the whole."""
     for table_line in db_connector.execute('SELECT * FROM switches'):
         print(table_line)
 
+
 def print_stars():
+    """Print 50 stars in the same line to make better output visibility."""
     print('\n', '*'*50, '\n', end='')
 
+
 print_stars()
-data_for_db = [(1, 'sw01', '2960', '1.1.1.1'), 
-            (2, 'sw02', '3750', '1.1.1.2'),
-            (3, 'sw03', '3850', '1.1.1.3'),
-            (4, 'sw04', '2960', '1.1.1.4')]
+
+data_for_db = [(1, 'sw01', '2960', '1.1.1.1'),
+               (2, 'sw02', '3750', '1.1.1.2'),
+               (3, 'sw03', '3850', '1.1.1.3'),
+               (4, 'sw04', '2960', '1.1.1.4')]
 
 db_connector = sqlite3.connect(os.path.join(sys.path[0], 'test2.db'))
 db_connector.execute('DROP TABLE IF EXISTS switches')
@@ -30,13 +39,16 @@ db_connector.commit()
 print_db_table()
 
 print_stars()
+
 new_to_db = "INSERT INTO switches VALUES(5, 'sw05', '2960', '1.1.1.5')"
 try:
     db_connector.execute(new_to_db)
 except sqlite3.IntegrityError as sql_error:
     print('DB error:', sql_error)
 db_connector.commit()
+
 print_db_table()
 
 print_stars()
+
 db_connector.close()
