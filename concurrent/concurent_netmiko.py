c2214e457e374927788feb6f9ed93b0daa869604
amelincpu <amelincpu@gmail.com>
2021-03-04 17:46:42 +0000

    4th of March 2021 update

--- concurrent/concurent_netmiko.py
+++ concurrent/concurent_netmiko.py
@@ -1,14 +1,19 @@
 """Testing Netmiko."""
 
 
+from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
 import netmiko
 import os
 import sys
 import yaml
 import logging
-from concurrent.futures import ThreadPoolExecutor
 
-logging.basicConfig(level=logging.INFO)
+
+logging.getLogger("paramiko").setLevel(logging.WARNING)
+logging.basicConfig(
+    level=logging.INFO)
+
 
 def print_stars():
     """Print 100 stars in the same line to make better output visibility."""
@@ -20,6 +25,7 @@ def send_ssh_show(ssh_device, ios_show):
     logging.info(f'Connecting to {ssh_device["ip"]}')
     with netmiko.ConnectHandler(**ssh_device) as ssh_session:
         ssh_session.enable()
+        logging.info(f'Got result from {ssh_device["ip"]}')
         return ssh_session.send_command(ios_show)
 
 
@@ -29,8 +35,8 @@ with open(os.path.join(sys.path[0], 'device_list.yaml')) as device_list_file:
     device_list = yaml.safe_load(device_list_file)
 
 with ThreadPoolExecutor(max_workers=3) as ssh_executor:
-    executor_result = ssh_executor.map(send_ssh_show, device_list, ['sh clock']*3)
-    for ssh_out in executor_result:
-        print(ssh_out)
+    executor_result = ssh_executor.map(send_ssh_show, device_list, repeat('sh clock'))
+    for device_list, ssh_out in zip(device_list, executor_result):
+        print('device ip :', device_list['ip'], 'time is', ssh_out)
 
 print_stars()
